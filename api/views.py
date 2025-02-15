71da9dde1d526ae9e37ae1fba11e4a8aa9b2a785
Csaba Ordog <ordog.csaba.0@gmail.com>
2025-02-15 03:38:22 +0100

    import django rest framework

---
+++ api/views.py
@@ -0,0 +1,21 @@
+from rest_framework.response import Response
+from rest_framwork.decorators import api_view
+
+@api_view(['POST'])
+def createTask(request):
+    pass
+
+
+@api_view(['GET'])
+def getTask(request):
+    pass
+
+
+@api_view(['PUT'])
+def updateTask(request):
+    pass
+
+
+@api_view(['DELETE'])
+def deleteTask(request):
+    pass
