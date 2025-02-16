1aaf6033d620b975859cf5a4bf03607865a80f69
Csaba Ordog <ordog.csaba.0@gmail.com>
2025-02-16 15:56:21 +0100

    add urls for create, update and delete task enpoints

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('<int:pk>/', views.getTask, name='get_task'),
     path('task-list/', views.getAllTasks, name='get_all_tasks'),
-
+    path('task-create/', views.createTask, name='create_task'),
+    path('task-update/<int:pk>/', views.updateTask, name='update_task'),
+    path('task-delete/<int:pk>/', views.deleteTask, name='delete_task'),
 ]
