e15a1e7eeb7cca398ab77ca8f56d926b975961c0
Csaba Ordog <ordog.csaba.0@gmail.com>
2025-02-16 09:05:53 +0100

    create Task model

---
+++ api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Task(models.Model):
+
+    class Status(models.TextChoices):
+        PENDING = 'PENDING', 'Pending'
+        IN_PROGRESS = 'IN_PROGRESS', "In Progress"
+        COMPLETED = 'COMPLETED', 'Completed'
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = model.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING
+        )
