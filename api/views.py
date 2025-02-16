3e6936e2bb4170244a82e4cca7bcd903eb4d053f
Csaba Ordog <ordog.csaba.0@gmail.com>
2025-02-16 15:57:17 +0100

    whitespaces

--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ from .serializers import TaskSerializer
 
 @api_view(['POST'])
 def createTask(request):
+
     serializer = TaskSerializer(data=request.data)
     
     if serializer.is_valid():
@@ -22,6 +23,7 @@ def getTask(request, pk):
     try:
         task = Task.objects.get(pk=pk)
         serializer = TaskSerializer(task)
+       
         return Response(serializer.data)
     
     except Task.DoesNotExist:
@@ -32,22 +34,23 @@ def getTask(request, pk):
 def getAllTasks(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
+
     return Response(serializer.data)
 
 
 @api_view(['PUT'])
 def updateTask(request, pk):
+    
     try:
         task = Task.objects.get(pk=pk)
     except Task.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    
     serializer = TaskSerializer(task, data=request.data)
 
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
-    
+
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
